eccedf9e411049abd9168ca6e853fdb0ccfa365e
Meera George <meera@cs.wisc.edu>
2017-06-23 16:52:07 -0500

    created a setup.py file

--- setup.py
+++ setup.py
@@ -25,63 +25,55 @@ def install_and_import(package):
         globals()[package] = importlib.import_module(package)
 
 
-# stall_and_import('setuptools')
+# Checking and installing the required packages
+install_and_import('pandas')
+install_and_import('numpy')
 
+# find packages to be included.
+packages = setuptools.find_packages()
 
-if __name__ == "__main__":
+with open('README.md') as f:
+    LONG_DESCRIPTION = f.read()
 
-    no_frills = (len(sys.argv) >= 2 and ('--help' in sys.argv[1:] or
-                                         sys.argv[1] in ('--help-commands',
-                                                         'egg_info', '--version',
-                                                         'clean')))
-
-    cwd = os.path.abspath(os.path.dirname(__file__))
-
-    # find packages to be included.
-    packages = setuptools.find_packages()
-
-    with open('README.rst') as f:
-        LONG_DESCRIPTION = f.read()
-
-    setuptools.setup(
-        name='profile_values',
-        version='0.2.1',
-        description='Python library for data profiling.',
-        long_description=LONG_DESCRIPTION,
-        url='',
-        author='Meera George',
-        author_email='meera@cs.wisc.edu',
-        license='BSD',
-        classifiers=[
-            'Development Status :: 4 - Beta',
-            'Environment :: Console',
-            'Intended Audience :: Developers',
-            'Intended Audience :: Science/Research',
-            'Intended Audience :: Education',
-            'License :: OSI Approved :: BSD License',
-            'Operating System :: POSIX',
-            'Operating System :: Unix',
-            'Operating System :: MacOS',
-            'Operating System :: Microsoft :: Windows',
-            'Programming Language :: Python',
-            'Programming Language :: Python :: 2',
-            'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 2.7',
-            'Programming Language :: Python :: 3.3',
-            'Programming Language :: Python :: 3.4',
-            'Programming Language :: Python :: 3.5',
-            'Topic :: Scientific/Engineering',
-            'Topic :: Utilities',
-            'Topic :: Software Development :: Libraries',
-        ],
-        packages=packages,
-        install_requires=[
-            'numpy >= 1.7.0',
-            'six'
-        ],
-        setup_requires=[
-            'numpy >= 1.7.0'
-        ],
-        include_package_data=True,
-        zip_safe=False
-    )
+setuptools.setup(
+    name='profile_values',
+    version='0.2.1',
+    description='Python library for data profiling.',
+    long_description=LONG_DESCRIPTION,
+    url='',
+    author='Meera George',
+    author_email='meera@cs.wisc.edu',
+    license='BSD',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Education',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Operating System :: MacOS',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Utilities',
+        'Topic :: Software Development :: Libraries',
+    ],
+    packages=packages,
+    install_requires=[
+        'numpy >= 1.7.0',
+        'six'
+    ],
+    setup_requires=[
+        'numpy >= 1.7.0'
+    ],
+    include_package_data=True,
+    zip_safe=False
+)
